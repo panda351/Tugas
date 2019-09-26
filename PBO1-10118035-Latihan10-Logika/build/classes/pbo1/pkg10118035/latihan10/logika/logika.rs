29797af84b793e925fc5103a05130170918cf709
GhoirizaWN <55840308+panda351@users.noreply.github.com>
2019-09-26 20:29:13 +0700

    Add files via upload

---
+++ PBO1-10118035-Latihan10-Logika/build/classes/pbo1/pkg10118035/latihan10/logika/logika.rs
@@ -0,0 +1,2 @@
+pbo1.pkg10118035.latihan10.logika.PBO110118035Latihan10Logika
+logika
---
+++ PBO1-10118035-Latihan10-Logika/src/pbo1/pkg10118035/latihan10/logika/logika.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ * Nama      : Ghoiriza Wahyu Nugraha
+ * Kelas     : PBO1
+ * NIM       : 10118035
+ * Deskripsi : Penggunaan tipe data boolean
+ */
+public class logika {
+    
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+}
