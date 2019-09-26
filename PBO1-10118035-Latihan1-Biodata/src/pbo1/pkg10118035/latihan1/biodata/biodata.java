686b80ab31acd692997eb8deddc9e874921d4975
GhoirizaWN <55840308+panda351@users.noreply.github.com>
2019-09-26 20:18:12 +0700

    Add files via upload

---
+++ PBO1-10118035-Latihan1-Biodata/src/pbo1/pkg10118035/latihan1/biodata/biodata.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ * Nama      : Ghoiriza Wahyu Nugraha
+ * Kelas     : PBO1
+ * NIM       : 10118035
+ * Deskripsi : Menampilkan biodata
+ */
+public class biodata {
+    
+    public static void main(String[] args){
+        System.out.println("Biodat" +"\n"+ "----------------------------");
+        System.out.println("Nama\t\t:Ghoiriza Wahyu Nugraha");
+        System.out.println("Alamat\t\t:Padalarang");
+        System.out.println("Tempat Lahir\t:Cimahi");
+        System.out.println("Tanggal Lahir\t:29 Febuari 2000");
+        System.out.println("----------------------------");
+        System.out.println("Hobi\t\t:Nonton Anime dan Game");
+        System.out.println("Makes\t\t:Ikiyaki dan Takoyaki");
+        System.out.println("Mikes\t\t:Macha");
+    }
+   
+}
