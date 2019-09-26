ff5f3e6ffeb2def06d9379d9d3e1a2fac82cc0ba
GhoirizaWN <55840308+panda351@users.noreply.github.com>
2019-09-26 20:27:57 +0700

    Add files via upload

---
+++ PBO1-10118035-Latihan9-Teks/src/pbo1/pkg10118035/latihan9/teks/teks.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ * Nama      : Ghoiriza Wahyu Nugraha
+ * Kelas     : PBO1
+ * NIM       : 10118035
+ * Deskripsi : Penggunaan tipe data teks
+ */
+public class teks {
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline ='\n';
+        c ='A';
+        System.out.println("Hasil" + newline + "Char : " + tab + "berisi " + c);
+        
+    }
+}
