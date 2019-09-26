74bfdb3670808945617f1214b91df9ee79e00f83
GhoirizaWN <55840308+panda351@users.noreply.github.com>
2019-09-26 20:31:07 +0700

    Add files via upload

---
+++ PBO1-10118035-Latihan13-Relational/build/classes/pbo1/pkg10118035/latihan13/relational/relational.rs
@@ -0,0 +1,2 @@
+pbo1.pkg10118035.latihan13.relational.PBO110118035Latihan13Relational
+relational
---
+++ PBO1-10118035-Latihan13-Relational/src/relational.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ * Nama      : Ghoiriza Wahyu Nugraha
+ * Kelas     : PBO1
+ * NIM       : 10118035
+ * Deskripsi : Penggunaan operator relational
+ */
+public class relational {
+    
+    public static void main(String[] args) {
+        int bil1 =10;
+        int bil2 =20;
+        System.out.println("a == b = "+ (bil1 == bil2));
+        System.out.println("a != b = "+ (bil1 != bil2));
+        System.out.println("a > b = "+ (bil1 > bil2));
+        System.out.println("a < b = "+ (bil1 < bil2));
+        System.out.println("a >= b = "+ (bil1 >= bil2));
+        System.out.println("a <= b = "+ (bil1 <= bil2));
+        
+    }
+}
