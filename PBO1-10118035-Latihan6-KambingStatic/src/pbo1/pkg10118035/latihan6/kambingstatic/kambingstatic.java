b3d67a7bc9b640008c4c66f69a7062c144deab26
GhoirizaWN <55840308+panda351@users.noreply.github.com>
2019-09-26 20:26:07 +0700

    Add files via upload

---
+++ PBO1-10118035-Latihan6-KambingStatic/src/pbo1/pkg10118035/latihan6/kambingstatic/kambingstatic.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118035.latihan6.kambingstatic;
+
+/**
+ *
+ * @author Aspire E 14
+ */
+public class kambingstatic {
+    //NamaKambing sebagai konstanta
+    public static final String Nama_Kambing ="Midun";
+    
+    public static void main(String[] args) {
+        mamalia.JumlahKambing = 485000;
+        System.out.println(Nama_Kambing + " Memiliki kambing sebanyak " +mamalia.JumlahKambing);
+        
+    }
+}
---
+++ PBO1-10118035-Latihan6-KambingStatic/src/pbo1/pkg10118035/latihan6/kambingstatic/mamalia.java
@@ -0,0 +1,20 @@
+package pbo1.pkg10118035.latihan6.kambingstatic;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * Nama      : Ghoiriza Wahyu Nugraha
+ * Kelas     : PBO1
+ * NIM       : 10118035
+ * Deskripsi : Penggunaan variabel static dan konstanta
+ */
+public class mamalia {
+    //Variabel jumlahkambing dideklarasikan sebagai statik
+    public static int JumlahKambing;
+}
