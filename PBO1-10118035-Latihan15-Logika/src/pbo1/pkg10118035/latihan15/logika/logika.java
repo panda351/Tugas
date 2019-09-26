d292026f73ddad95e2312e43332668a4af4e12a7
GhoirizaWN <55840308+panda351@users.noreply.github.com>
2019-09-26 20:32:53 +0700

    Add files via upload

---
+++ PBO1-10118035-Latihan15-Logika/src/pbo1/pkg10118035/latihan15/logika/logika.java
@@ -0,0 +1,27 @@
+package pbo1.pkg10118035.latihan15.logika;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ * Nama      : Ghoiriza Wahyu Nugraha
+ * Kelas     : PBO1
+ * NIM       : 10118035
+ * Deskripsi : Penggunaan operator logika
+ */
+public class logika {
+    
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = "+ (a && b));
+        System.out.println("a || b = "+ (a || b));
+        System.out.println("!a = "+ !a);
+        System.out.println("!(a && b) = " + !(a &&b));
+    }
+}
